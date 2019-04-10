386bdb16d11ff3de65afb7f3704d2b40e8705262
Raul Martinez <raulmartinez@Rauls-MacBook-Pro.local>
2019-04-10 19:59:39 -0400

    Removed Fragment import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import '../sass/App.scss';
 import data, { randomId } from '../data/tasks';
 import Task from './Task';
